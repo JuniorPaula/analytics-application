b347c1bd8479f22a49fc77692ba34172a18a1e7c
Junior Paula <luke.junnior@icloud.com>
2023-07-02 11:25:26 -0300

    feat: implements gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,14 @@ import (
 	"c2d-reports/internal/router"
 	"c2d-reports/pkg/jobs"
 	"c2d-reports/pkg/rabbitmq"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -17,18 +22,44 @@ func main() {
 	// start the consumer on reports queue in a goroutine
 	go rabbitmq.ConsumerOnReportsQueue()
 
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	// create the context to control of the exit
+	_, cancel := context.WithCancel(context.Background())
+
+	// create the server
+	server := &http.Server{Addr: fmt.Sprintf(":%d", config.Port), Handler: router.Handler()}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error while start the server: %v", err)
+		}
+	}()
+
 	// print out the port
 	fmt.Println("[::] Starting server on the port: ", config.Port)
-
-	// initialize the router
-	r := router.Handler()
 
 	// schedule the jobs
 	startSchedule()
 
-	// start the server
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
+	// wait for the interrupt signal
+	<-interrupt
+	log.Println("[::] Shutting down the server...")
 
+	// cancel the context
+	cancel()
+
+	// wait for to shutdown the server
+	timeout := 5 * time.Second
+	ctxShutdown, cancelShutdown := context.WithTimeout(context.Background(), timeout)
+	defer cancelShutdown()
+
+	// shutdown the server
+	if err := server.Shutdown(ctxShutdown); err != nil {
+		log.Fatalf("error while shutdown the server: %v", err)
+	}
+
+	log.Println("[::] Server gracefully stopped")
 }
 
 // startSchedule schedules the jobs
