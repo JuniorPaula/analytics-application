aa4b0499b2be42370c8bf648f0d53e4cc80a6835
Junior Paula <luke.junnior@icloud.com>
2023-07-26 16:30:25 -0300

    feat: implements  handler to create a company

---
+++ internal/handlers/company_handlers.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"c2d-reports/internal/entity"
+	"c2d-reports/internal/usecases/companies"
+	"c2d-reports/internal/utils"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func CreateCompany_hanlder(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	var company entity.Company
+	err = json.Unmarshal(body, &company)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	company, err = companies.CreateCompanyUsecase(company)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, company)
+}
